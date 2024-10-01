08153a9b9dad4bd1115955db954c3c37814fc9e3
kirill-83 <kirill.pankov.83@mail.ru>
2024-10-01 15:22:54 +0700

    Add files via upload

---
+++ Орг. программ.py
@@ -0,0 +1,6 @@
+name = input("Введите Ваше имя: ")
+current_year = 2024
+date_of_birth = int(input("В Каком году Вы родились? "))
+age = current_year - date_of_birth
+print("Здравствуйте,", name)
+print("В этом году Вам" , age , "лет")
